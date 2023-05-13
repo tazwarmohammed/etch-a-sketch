9b0c6dfd9bdf2687127bc5b6aa90f8909fb80552
Tazwar Mohammed <tazwarmohammed1998@gmail.com>
2023-05-14 01:42:47 +0600

    Implement 'set' button that varies palette dimensions

--- main.js
+++ main.js
@@ -1,20 +1,52 @@
 const container = document.querySelector('.container');
 
-for (let index = 0; index < 256; index++) {
-    const child = document.createElement('div');
-    child.classList.add('initial-color');
-    child.textContent = index;
-    child.addEventListener('mouseover', () => {
-        child.classList.add('change-color');
-    });
-    container.appendChild(child);
+let dimension = 16;
+
+function setPalette(dimension) {
+    removePalette();
+    container.setAttribute('style', `grid-template: repeat(${dimension}, 1fr) / repeat(${dimension}, 1fr);`);
+    for (let index = 1; index <= dimension * dimension; index++) {
+        const child = document.createElement('div');
+        child.classList.add('initial-color');
+        // child.textContent = index;
+        // child.setAttribute('style', 'font-size: 20px')
+        child.addEventListener('mouseover', () => {
+            child.classList.add('change-color');
+        });
+        container.appendChild(child);
+    }
 }
 
-const button = document.querySelector('button');
+function removePalette() {
+    const children = document.querySelectorAll('.initial-color');
+    children.forEach((child) => {
+        child.remove();
+    });
+}
 
-button.addEventListener('click', ()=> {
+
+const setBtn = document.querySelector('.set');
+
+setBtn.addEventListener('click', () => {
+    do {
+        if(dimension > 100) {
+            dimension = prompt('Max square limit exceeded, enter a number below 100')
+        } else {
+            dimension = prompt('Please enter number of squares on each side of palette');
+        }
+    } while (dimension > 100);
+    setPalette(dimension);
+});
+
+
+const resetBtn = document.querySelector('.reset');
+
+resetBtn.addEventListener('click', () => {
     const children = document.querySelectorAll('.change-color');
     children.forEach((child) => {
         child.classList.remove('change-color');
     })
 });
+
+
+setPalette(dimension);
